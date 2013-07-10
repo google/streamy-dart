a2708a188f2bba4a59d48a9c8315cb7268c865cd
Geoff Adams <gadams@google.com>
2013-07-09 18:56:48 -0700

    Throw NoSuchMethodError if the caller requests a nonexistent member
    Angular--and potentially other callers--expect that when a method such
    as containsKey is called on a DynamicEntity, that the object will throw
    NoSuchMethodError. DynamicEntity, on the other hand, wants to ensure
    that no one stashes a completion in a DynamicEntity and tries to call
    it later. This patch accomodates both needs.

--- lib/runtime/entity/dynamic.dart
+++ lib/runtime/entity/dynamic.dart
@@ -19,7 +19,21 @@ class DynamicEntity extends RawEntity {
       var key = memberName.substring(0, memberName.length - 1);
       this[key] = invocation.positionalArguments[0];
     } else {
-      throw new ClosureInvocationException(memberName);
+      // Perhaps the called was trying to call a nonexistent method,
+      // or perhaps the caller was trying to invoke a data member as a
+      // completion. Throw an appropriate error.
+      if (contains(memberName)) {
+        throw new ClosureInvocationException(memberName);
+      } else {
+        // The Invocation class and NoSuchMethodError have different ideas
+        // about the type of the named arguments map.
+        Map<String, dynamic> namedArgs = {};
+        for (key in invocation.namedArguments.keys) {
+          namedArgs[key.toString()] = invocation.namedArguments[key];
+        }
+        throw new NoSuchMethodError(this, memberName,
+            invocation.positionalArguments, namedArgs);
+      }
     }
   }
 
