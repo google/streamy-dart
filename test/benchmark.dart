ab68de5373ef96ebccd72d11beec367e641492ad
Alex Rickabaugh <alx+alxhub@alxandria.net>
2013-09-16 13:23:04 -0700

    Basic benchmark for deserialization.

---
+++ test/benchmark.dart
@@ -0,0 +1,51 @@
+library streamy.benchmarks;
+
+import 'dart:json' as json;
+import 'dart:math';
+
+import 'package:benchmark_harness/benchmark_harness.dart';
+import 'package:fixnum/fixnum.dart';
+import 'generated/benchmark_client.dart';
+
+const MAX = 10000000;
+const REPL = 35;
+const LEVEL = 3;
+
+class DeserializationBenchmark extends BenchmarkBase {
+  
+  final Random _random = new Random(1234);
+  var js;
+
+  static void main() {
+    new DeserializationBenchmark().report();
+  }
+  
+  DeserializationBenchmark() : super("Deserialization");
+  
+  void run() {
+    var foo = new Foo.fromJsonString(js);
+  }
+  
+  void setup() {
+    var foo = _makePopulatedFoo(LEVEL);
+    js = json.stringify(foo);
+  }
+  
+  Foo _makePopulatedFoo(int level) {
+    Foo foo = new Foo()
+      ..id = _random.nextInt(MAX)
+      ..baz = _random.nextInt(MAX)
+      ..qux = new Int64.fromInts(_random.nextInt(MAX), _random.nextInt(MAX))
+      ..quux = new List.generate(_random.nextInt(REPL), (_) => _random.nextDouble(), growable: false)
+      ..corge = new List.generate(_random.nextInt(REPL), (_) => _random.nextInt(MAX), growable: false)
+      ..bar = new Bar();
+    if (level > 0) {
+      foo.bar.foos = new List.generate(REPL, (_) => _makePopulatedFoo(level - 1), growable: false);
+    }
+    return foo;
+  }
+}
+
+main() {
+  DeserializationBenchmark.main();
+}
