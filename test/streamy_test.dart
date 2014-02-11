bd6d946346e862b7ae7af55d6a8dea4c2c411001
Yegor <yjbanov@google.com>
2014-02-10 22:45:49 -0800

    Move all RawEntity tests to raw_test.dart

--- test/streamy_test.dart
+++ test/streamy_test.dart
@@ -8,29 +8,6 @@ import 'package:streamy/testing/testing.dart';
 import 'package:unittest/unittest.dart';
 
 main() {
-  group('RawEntity', () {
-    test('factory constructor returns a RawEntity', () {
-      expect(new Entity(), new isInstanceOf<RawEntity>());
-    });
-    test('factory constructor fromMap returns a RawEntity', () {
-      expect(new Entity.fromMap({'foo': 'bar'}), new isInstanceOf<RawEntity>());
-    });
-    test('does not allow setting closures on non-.local keys', () {
-      var e = new RawEntity();
-      expect(() => e['foo'] = () => false, throwsA(new isInstanceOf<ClosureInEntityException>()));
-    });
-    test('does allow setting closures on .local', () {
-      var e = new RawEntity();
-      e['local.foo'] = () => true;
-    });
-    test('.wrapMap constructor does not copy data', () {
-      var map = toObservable({'list': [1, 2, 3]});
-      var e = new RawEntity.wrapMap(map);
-      map['list'].add(4);
-      expect(e['list'].length, equals(4));
-      expect(e['list'][3], equals(4));
-    });
-  });
   group('DynamicEntity', () {
     test('noSuchMethod getters/setters work', () {
       var e = new DynamicEntity();
@@ -41,36 +18,6 @@ main() {
       var e = new DynamicEntity();
       e.foo = 'a';
       expect(() => e.foo(), throwsA(new isInstanceOf<ClosureInvocationException>()));
-    });
-  });
-  group('.local', () {
-    var entity;
-    setUp(() {
-      entity = new RawEntity();
-    });
-    test('exists and is a Map', () {
-      expect(entity.local, isNotNull);
-      expect(entity.local, new isInstanceOf<Map>());
-    });
-    test('stores and retrieves data via operator[]', () {
-      entity.local['foo'] = 'bar';
-      expect(entity.local['foo'], equals('bar'));
-    });
-    test('does not affect serialization of the entity', () {
-      var s1 = entity.toJson();
-      entity.local['foo'] = 'not serialized';
-      var s2 = entity.toJson();
-      expect(s2, equals(s1));
-    });
-    test('does not survive cloning', () {
-      entity.local['foo'] = 'this should not be cloned';
-      expect(entity.clone().local['foo'], isNull);
-    });
-    test('cannot be set', () {
-      expect(() => entity.local = {},
-          throwsA(new isInstanceOf<NoSuchMethodError>()));
-      expect(() => entity['local'] = {},
-          throwsA(new isInstanceOf<ArgumentError>()));
     });
   });
   group('jsonParse', () {
