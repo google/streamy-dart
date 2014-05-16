838a7ebbad70e6704eae91a89bb9ed5b74103268
Alex Rickabaugh <alx+alxhub@alxandria.net>
2014-05-16 12:19:56 -0700

    Remove generated code which is now served with Barback transformers.

--- test/generated/benchmark_client_requests.dart
+++
@@ -1,9 +0,0 @@
-/**
- * WARNING: GENERATED CODE. DO NOT EDIT BY HAND.
- * 
- */
-library schemaobjecttest.requests;
-import 'dart:async';
-import 'package:fixnum/fixnum.dart' as fixnum;
-import 'package:streamy/streamy.dart' as streamy;
-import 'benchmark_client_objects.dart' as obj;
