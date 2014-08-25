9fb4c39d5b67da432f397c795d3f785a412fb7e9
Douglas Gresham <douglasgresham@google.com>
2014-08-25 15:00:54 +0100

    Support sub-resources in discovery API generation

--- test/all_tests.dart
+++ test/all_tests.dart
@@ -6,6 +6,7 @@ import 'generated/addendum_test.dart' as generated_addendum_test;
 import 'generated/handler_test.dart' as generated_handler_test;
 import 'generated/identifier_name_test.dart' as generated_identifier_name_test;
 import 'generated/method_get_test.dart' as generated_method_get_test;
+import 'generated/nested_resources_test.dart' as generated_nested_resources_test;
 import 'generated/method_post_test.dart' as generated_method_post_test;
 import 'generated/method_params_test.dart' as generated_method_params_test;
 import 'generated/proto_test.dart' as generated_proto_test;
@@ -45,6 +46,7 @@ main(List<String> args) {
   generated_method_get_test.main();
   generated_method_post_test.main();
   generated_method_params_test.main();
+  generated_nested_resources_test.main();
   generated_proto_test.main();
   generated_schema_object_test.main();
 
