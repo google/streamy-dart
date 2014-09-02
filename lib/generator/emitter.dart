4b497d8ada066a1446ba60628eca0db7cc23f5a0
yjbanov <yjbanov@google.com>
2014-09-02 16:05:28 -0700

    moved common emitter methods into EmitterBase; defined EmitterContext and MarshallerEmitter interfaces

--- lib/generator/emitter.dart
+++ lib/generator/emitter.dart
@@ -4,6 +4,8 @@ import 'dart:async';
 import 'package:mustache/mustache.dart' as mustache;
 import 'package:streamy/generator/config.dart';
 import 'package:streamy/generator/dart.dart';
+import 'package:streamy/generator/emitter_util.dart';
+import 'package:streamy/generator/generator.dart';
 import 'package:streamy/generator/ir.dart';
 import 'package:streamy/generator/util.dart';
 
@@ -33,13 +35,12 @@ class Emitter {
   Emitter(this.config, this._templates);
 
   StreamyClient process(Api api) {
-    final ctx = new EmitterContext(config, _templates, api);
+    final ctx = new _EmitterContext(config, _templates, api);
     return ctx.process();
   }
 }
 
-class EmitterContext {
-  static final BASE_PREFIX = '_streamy_base_';
+class _EmitterContext extends EmitterBase implements EmitterContext {
 
   final Config config;
   final Map<String, mustache.Template> templates;
@@ -71,7 +72,7 @@ class EmitterContext {
   DartFile get dispatchFile => _dispatchFile;
   String get dispatchPrefix => _dispatchPrefix;
 
-  EmitterContext(this.config, this.templates, this.api) {
+  _EmitterContext(this.config, this.templates, this.api) {
     _libPrefix = api.name;
     if (api.httpConfig != null) {
       _libPrefix = "$_libPrefix";
@@ -745,63 +746,6 @@ class EmitterContext {
         isStatic: true, isFinal: true));
     clazz.fields.add(new DartComplexField.getterOnly('apiType',
         const DartType.string(), new DartConstantBody("=> r'${clazz.name}';")));
-  }
-
-  DartBody stringListBody(Iterable<String> strings, {bool getter: false}) =>
-      new DartTemplateBody(_template('string_list'), {
-        'list': strings.map((i) => {'value': i}).toList(growable: false),
-        'getter': getter
-      });
-
-  DartBody mapBody(Map<String, String> map) {
-    var data = [];
-    map.forEach((key, value) {
-      data.add({'key': key, 'value': value});
-    });
-    return new DartTemplateBody(_template('string_map'), {'map': data});
-  }
-  
-  Map invertMap(Map input) {
-    Map output = {};
-    input.forEach((key, value) {
-      output[value] = key;
-    });
-    return output;
-  }
-
-  DartType streamyImport(String clazz, {params: const []}) =>
-      new DartType(clazz, 'streamy', params);
-
-  DartType toDartType(TypeRef ref, {bool withPrefix: true}) {
-    if (ref is ListTypeRef) {
-      return new DartType.list(toDartType(ref.subType));
-    } else if (ref is SchemaTypeRef) {
-      final prefix = withPrefix ? objectPrefix : null;
-      return new DartType(makeClassName(ref.schemaClass), prefix, const []);
-    } else {
-      switch (ref.base) {
-        case 'int64':
-          return new DartType('Int64', 'fixnum', const []);
-        case 'integer':
-          return const DartType.integer();
-        case 'string':
-          return const DartType.string();
-        case 'any':
-          return const DartType.dynamic();
-        case 'double':
-          return const DartType.double();
-        case 'boolean':
-          return const DartType.boolean();
-        case 'number':
-          return const DartType.double();
-        case 'external':
-          ExternalTypeRef externalTypeRef = ref;
-          return new DartType(externalTypeRef.type,
-              externalTypeRef.importedFrom, const []);
-        default:
-          throw new Exception('Unhandled API type: $ref');
-      }
-    }
   }
 
   mustache.Template _template(String name) => templates[name];
---
+++ lib/generator/generator.dart
@@ -0,0 +1,32 @@
+library streamy.generator.generator;
+
+import 'package:mustache/mustache.dart' as mustache;
+import 'package:streamy/generator/config.dart';
+import 'package:streamy/generator/dart.dart';
+import 'package:streamy/generator/ir.dart';
+
+/// Carries the state of a single run of the emitter.
+abstract class EmitterContext {
+  Config get config;
+  Map<String, mustache.Template> get templates;
+  Api get api;
+  String get libPrefix;
+
+  DartLibrary get rootFile;
+  DartFile get resourceFile;
+  DartFile get requestFile;
+  DartFile get objectFile;
+  DartFile get dispatchFile;
+
+  String get rootPrefix;
+  String get resourcePrefix;
+  String get requestPrefix;
+  String get objectPrefix;
+  String get dispatchPrefix;
+}
+
+/// Emits a specific implementation of a marshaller into a given
+/// [EmitterContext].
+abstract class MarshallerEmitter {
+  void emit(EmitterContext context);
+}
