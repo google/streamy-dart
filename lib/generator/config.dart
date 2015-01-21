74a9084a2d24adf0d1251c2be6136bb4eb71a4ae
Alex Rickabaugh <alx+alxhub@alxandria.net>
2015-01-21 14:45:28 -0800

    Remove config.lazy.

--- lib/generator/config.dart
+++ lib/generator/config.dart
@@ -90,7 +90,6 @@ class Config {
   bool removers = true;
   bool known = false;
   bool global = false;
-  bool lazy = false;
   List<SendParam> sendParams = [];
   
   Config();
@@ -223,9 +222,6 @@ Config parseConfigOrDie(Map data) {
     }
     if (options.containsKey('patch')) {
       config.patch = options['patch'];
-    }
-    if (options.containsKey('lazy')) {
-      config.lazy = options['lazy'];
     }
   }
   
