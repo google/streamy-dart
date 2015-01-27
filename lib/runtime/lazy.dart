87dea7dd0ef79677c51a6985df8a2342e4ac7dde
Alex Rickabaugh <alx+alxhub@alxandria.net>
2015-01-27 15:17:53 -0800

    Changes for code review.

--- lib/runtime/lazy.dart
+++ lib/runtime/lazy.dart
@@ -1,21 +1,28 @@
 part of streamy.runtime;
 
-/// A [Lazy] represents a value with a high cost of deserialization. 
+typedef T LazyDeserializerFn<T>(rawValue);
+
+/// A [Lazy] represents a value of type T with a high cost of deserialization.
 class Lazy<T> {
   
-  var protoValue, process;
+  var rawValue;
+  LazyDeserializerFn<T> deserializer;
 
-  Lazy(this.protoValue, this.process);
+  /// Construct a lazily deserialized value from a raw value and a closure
+  /// to deserialize it.
+  Lazy(this.rawValue, this.deserializer);
   
-  T resolve() => process(protoValue);
+  T resolve() => deserializer(rawValue);
   
-  static toLazy(process) => ((v) => new Lazy(v, process));
+  static toLazy(LazyDeserializerFn deserializer) =>
+      ((v) => new Lazy(v, deserializer));
 }
 
 // An [ObservableList] that understands [Lazy] values and silently resolves them.
 class LazyList<E> extends ListBase<E> implements ObservableList<E> {
   
-  // Not ObservableList<E> since this can contain Lazy elements which aren't of type E.
+  // Not ObservableList<E> since this can contain Lazy elements which aren't of
+  // type E.
   ObservableList delegate;
   bool _changesDisabled = false;
   StreamController<List<ChangeRecord>> _changes;
@@ -37,7 +44,8 @@ class LazyList<E> extends ListBase<E> implements ObservableList<E> {
       // Replace the resolved value in the list.
       delegate[index] = value;
       
-      // Make sure the replacement change has been discarded before enabling changes again.
+      // Make sure the replacement change has been discarded before enabling
+      // changes again.
       deliverChanges();
       _changesDisabled = false;
     }
@@ -49,7 +57,8 @@ class LazyList<E> extends ListBase<E> implements ObservableList<E> {
     delegate[index] = value;
   }
   
-  // Manually delegating [add] and [addAll] as per [ListBase] performance recommendations.
+  // Manually delegating [add] and [addAll] as per [ListBase] performance
+  // recommendations.
   void add(E value) => delegate.add(value);
   void addAll(Iterable<E> iterable) => delegate.addAll(iterable);
   
@@ -60,7 +69,8 @@ class LazyList<E> extends ListBase<E> implements ObservableList<E> {
   void discardListChanges() => delegate.discardListChages();
   
   void notifyChange(ChangeRecord record) => delegate.notifyChange(record);
-  notifyPropertyChange(Symbol field, Object oldValue, Object newValue) => delegate.notifyPropertyChange(oldValue, newValue);
+  notifyPropertyChange(Symbol field, Object oldValue, Object newValue) =>
+      delegate.notifyPropertyChange(oldValue, newValue);
   void observed() => delegate.observed();
   void unobserved() => delegate.unobserved();
   
